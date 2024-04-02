42bbd3c141e59c835cb6c428f6ad4cb469cd0cc9
althafaz <52558189+althafaz@users.noreply.github.com>
2024-04-01 21:55:01 -0400

    "Data-type - Studio exercise is completed"

---
+++ datatypes/datatypes-studio/src/main/java/org.launchcode/Area.java
@@ -0,0 +1,29 @@
+package org.launchcode;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Area {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double radius = 0;
+
+
+        while(radius <= 0) {
+
+           try {
+               System.out.print("Radius of the circle: ");
+               radius = input.nextDouble();
+
+                if(radius > 0){
+                    double calArea = Circle.getArea(radius);
+                    System.out.println("Area of circle is " + calArea);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Double value is not value");
+                break;
+            }
+        }
+    }
+
+}
